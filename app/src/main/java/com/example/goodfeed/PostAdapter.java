7fe40089317497fe9a03802d103ab971ade928f0
uditbhaskar <bhaskarudit8@gmail.com>
2020-02-27 22:14:36 +0530

    RecyclerView logi

--- app/src/main/java/com/example/goodfeed/PostAdapter.java
+++ app/src/main/java/com/example/goodfeed/PostAdapter.java
@@ -11,7 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.GlideBuilder;
+
 
 public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostHolder> {
 
